f36c006ba4cbceebd80c6bb2431a4b8dfcf3281a
CCChang1996 <44011694+CCChang1996@users.noreply.github.com>
2019-07-16 20:13:01 +0800

    Add files via upload

---
+++ 函数.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 19:41:00 2019
+
+@author: 13486
+"""
+
+# 定义函数
+def mysum(x, y):
+    return x+y
+# 测试函数
+result = mysum(x=1, y=2)
+print(result)
+
+#with语句
+#with open('somefileName') as somefile:
+#    for line in somefile:
+#       print(line)
+        
+       
